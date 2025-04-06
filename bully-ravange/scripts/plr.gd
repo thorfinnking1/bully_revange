7b745605e32ccb2dcffcd80c91f72a11b448d134
thorfinnking1 <157901908+thorfinnking1@users.noreply.github.com>
2025-04-06 21:04:35 +0300

    movement

---
+++ bully-ravange/scripts/plr.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+const SPEED = 200.0
+
+
+func _physics_process(delta: float) -> void:
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var up_down = Input.get_axis("ui_up", "ui_down")
+	if up_down:
+		velocity.y = up_down * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	move_and_slide()
