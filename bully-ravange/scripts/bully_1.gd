74fa59fb91c0032e14e36f5b96f11c1369e06b1e
MrZece <domnulx7@gmail.com>
2025-04-09 12:48:07 +0300

    fuckmihorny

--- bully-ravange/scripts/bully_1.gd
+++ bully-ravange/scripts/bully_1.gd
@@ -11,6 +11,19 @@ func _ready() -> void:
 	choosepoint(points)
 
 func _physics_process(_delta: float) -> void:
+	var nextp=nav.get_next_path_position()
+	if nav.is_navigation_finished():
+		choosepoint(points)
+	_on_navigation_agent_2d_velocity_computed((nextp-global_position).normalized()*200)
+	move_and_slide()
+
+func choosepoint(poins):
+	var n = randi_range(0,poins.get_child_count()-1)
+	targetpos=poins.get_child(n).global_position
+	nav.target_position=targetpos
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
 	if velocity:
 		ani.play("walking")
 		if velocity.x < 0:
@@ -19,16 +32,3 @@ func _physics_process(_delta: float) -> void:
 		else:
 			$Skeleton2D.scale.x = 1
 			$polygons.scale.x = 1.141
-	else:
-		ani.play("idle")
-	
-	if nav.is_navigation_finished():
-		choosepoint(points)
-	var nextp=nav.get_next_path_position()
-	velocity=(nextp-global_position).normalized()*200
-	move_and_slide()
-
-func choosepoint(poins):
-	var n = randi_range(0,poins.get_child_count()-1)
-	targetpos=poins.get_child(n).global_position
-	nav.target_position=targetpos
